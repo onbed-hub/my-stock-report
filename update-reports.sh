f5ebd5f91d68e223a9106945e9163a98bb740203
onbed-hub <danceonbed@gmail.com>
2026-04-28 11:11:25 +0800

    Auto-sync (Last 1 days): 2026-04-28 11:11:25

--- update-reports.sh
+++ update-reports.sh
@@ -34,8 +34,8 @@ for dir_path in $(find "$SOURCE_BASE" -maxdepth 1 -type d -regextype sed -regex 
         
         # 處理 HTML
         find "$dir_path" -maxdepth 1 -type f -name "*.html" \
-            ! -name "*live-[0-9]*" \
-            ! -name "*analysis-[0-9]*" \
+#            ! -name "*live-[0-9]*" \
+#            ! -name "*analysis-[0-9]*" \
             ! -name "[0-9]*" \
             -exec cp -u {} "$TARGET_BASE/$dir_name/" \; 2>/dev/null
 
